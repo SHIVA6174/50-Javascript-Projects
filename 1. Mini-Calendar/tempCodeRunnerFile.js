a095880f1130fd263459239eeeaad0f88621c047
Shiva WebDev <psivaiah2003@gmail.com>
2025-03-27 08:54:44 +0530

    Mini-Calendar

---
+++ 1. Mini-Calendar/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+ementById("month-name");
+// const dayName = document.getElementById("day-name");
+// const dayNumber = document.getElementById("day-number");
+// const year = document.getElementById("year");
